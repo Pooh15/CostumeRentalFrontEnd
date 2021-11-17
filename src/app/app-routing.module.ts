36e12c8d54c4e7b2346b9b67b00678ae5391fca6
Varsha Patil <varsha@Varshas-Air.attlocal.net>
2021-11-17 12:28:27 -0500

    added browsing page by varsha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InventoryComponent } from './browse/inventory/inventory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'browse/inventory', component: InventoryComponent },
+  { path: 'pattern', component: InventoryComponent },
+  { path: 'clothmaterial', component: InventoryComponent },
+  { path: 'size', component: InventoryComponent },
+  { path: 'color', component: InventoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,50 @@ import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpErrorHandler } from './services/http-error-handler.service';
 import { MessageService } from './services/message.service';
+import { MatButtonModule } from '@angular/material/button';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home/home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { InventoryComponent } from './browse/inventory/inventory.component';
 import {InventoryformService} from './services/inventoryform.service';
 import {InventoryService} from './services/inventory.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
   AppComponent,
+  NavComponent,
   HeaderComponent,
-  HomeComponent
+  HomeComponent,
+  InventoryComponent
   ],
   imports: [
   BrowserModule,
   AppRoutingModule,
   ReactiveFormsModule,
   FormsModule,
-  HttpClientModule
+  HttpClientModule,
+  BrowserAnimationsModule,
+  MatButtonModule,
+  LayoutModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule
   ],
   providers: [MessageService, InventoryformService, HttpErrorHandler, InventoryService],
   bootstrap: [AppComponent]
