bd11404b5948057649aae3b56cd36f4d37832451
sasneprasanna <psasne@oakland.edu>
2021-11-17 01:21:30 -0500

    login/register UI

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers:[DatePipe]
 })
 export class HeaderComponent implements OnInit {
+  today: number = Date.now(); //yyyy-mm-dd
+  constructor(private datePipe: DatePipe) { 
 
-  constructor() { }
+  console.log( this.datePipe.transform(this.today, 'yyyy-MM-dd'));
+  }
 
   ngOnInit(): void {
   }
